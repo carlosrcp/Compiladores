8c90b1067bef7e139cb55ec660e28ff50672d878
carlosrcp <86299990+carlosrcp@users.noreply.github.com>
2023-04-05 22:40:54 -0300

    entrega task1

--- task1.py
+++ task1.py
@@ -1,1 +1,36 @@
+# o arquivo Calc1.stk é input do compilador
+class RPNStacker:
+    def __init__(self):
+        self.stack = []
+        self.inputs = []
 
+    def start(self):
+        with open('Calc1.stk', 'r') as f:
+            self.inputs = f.read().split()
+
+        print(self.inputs)
+        while(len(self.inputs)>0):
+            if self.inputs[0].isdigit():
+                self.stack.append(int(self.inputs.pop(0)))
+            elif len(self.stack) >= 2:
+                op2 = self.stack.pop()                
+                op1 = self.stack.pop()
+                operator = self.inputs.pop(0)
+                if operator == '+':
+                    self.stack.append(op1+op2)
+                elif operator == '-':
+                    self.stack.append(op1-op2)
+                elif operator == '*':
+                    self.stack.append(op1*op2)
+                elif operator == '/':
+                    self.stack.append(op1/op2)
+            else :
+                print("erro!")
+                return
+        
+        print(self.stack[0])
+
+
+comp = RPNStacker()
+
+comp.start()
